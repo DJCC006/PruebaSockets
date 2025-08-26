8b41c2ed612e015bad967db0f5d1e6a13f4770d1
DJCC <david.carcamo2006@gmail.com>
2025-08-26 11:46:03 -0600

    creacion de repositorio de prueba

---
+++ src/main/java/com/mycompany/server_3/Server_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.server_3;
+
+/**
+ *
+ * @author David
+ */
+public class Server_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
